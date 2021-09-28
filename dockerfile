f9f459d1e193fbc00670ff559e9d38816f0f6b76
Maxim Umnov <maximumnoff@gmail.com>
2021-09-28 19:28:13 +0400

    first commit

--- dockerfile
+++ dockerfile
@@ -5,7 +5,7 @@ COPY 	000-default.conf /etc/apache2/sites-available/
 COPY	ports.conf /etc/apache2/
 #-------Установка модулей PostgreSQL-------
 RUN	    apt-get update -y
-RUN	    apt-get install libpq-dev -y
+RUN	    apt-get install libpq-dev iputils-ping net-tools dnsutils -y
 RUN     docker-php-ext-install pdo
 RUN 	docker-php-ext-install pdo_pgsql 
 RUN 	docker-php-ext-install pgsql
--- php_page/dbconnect.php
+++ php_page/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-    $postgre = pg_connect("host=pghost port=5432 user=postgres password=1234 dbname=postgres");
+    $postgre = pg_connect("host=website-db.student-project.svc.cluster.local port=5432 user=postgres password=1234 dbname=postgres");
     //-----------------Проверяем, успешность соединения-----------------
     if (!$postgre) {
         die("<p><strong>Ошибка подключения к БД</strong></p><p><strong>Код ошибки: </strong> ". $postgre->connect_errno ." </p><p><strong>Описание ошибки:</strong> ".$postgre->connect_error."</p>");
